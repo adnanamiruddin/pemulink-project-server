2f11b82e50585ba94e426f623b6664a9f5aaea1b
adnanamiruddin <adnan.amiruddin34@gmail.com>
2024-01-03 21:25:17 +0800

    feat: Create missionAcceptanceReq route and add it to routes index

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import usersRoute from "./users.route.js";
 import misssionsRoute from "./missions.route.js";
+import missionAcceptanceReqsRoute from "./missionAcceptanceReqs.route.js";
 
 const router = express.Router();
 
 router.use("/users", usersRoute);
 router.use("/missions", misssionsRoute);
+router.use("/mission-acceptance-reqs", missionAcceptanceReqsRoute);
 
 export default router;
---
+++ src/routes/missionAcceptanceReqs.route.js
@@ -0,0 +1,42 @@
+import express from "express";
+import { body } from "express-validator";
+import requsetHandler from "../handlers/request.handler.js";
+import tokenMiddleware from "../middlewares/token.middleware.js";
+import missionAcceptanceReqs from "../controllers/missionAcceptanceReqs.controller.js";
+
+const router = express.Router();
+
+router.post(
+  "/:missionId",
+  tokenMiddleware.auth,
+  missionAcceptanceReqs.createMissionAcceptanceReq
+);
+
+router.get(
+  "/",
+  tokenMiddleware.auth,
+  missionAcceptanceReqs.getAllMissionAcceptanceReqs
+);
+
+router.get(
+  "/:id",
+  tokenMiddleware.auth,
+  missionAcceptanceReqs.getMissionAcceptanceReqById
+);
+
+router.put(
+  "/:id",
+  [
+    body("status")
+      .notEmpty()
+      .withMessage("Status is required")
+      .isIn(["approved", "rejected"])
+      .withMessage("Invalid status"),
+    body("message").notEmpty().withMessage("Message is required"),
+  ],
+  requsetHandler.validate,
+  tokenMiddleware.auth,
+  missionAcceptanceReqs.updateMissionAcceptanceReq
+);
+
+export default router;
