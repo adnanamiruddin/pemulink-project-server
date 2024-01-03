5b27ac22a6ed95e30b908a0adfab1bb57130f5c5
adnanamiruddin <adnan.amiruddin34@gmail.com>
2024-01-03 21:14:28 +0800

    feat: Create users route and add it to routes index

---
+++ src/routes/users.route.js
@@ -0,0 +1,36 @@
+import express from "express";
+import usersController from "../controllers/users.controller.js";
+import { body } from "express-validator";
+import requsetHandler from "../handlers/request.handler.js";
+import tokenMiddleware from "../middlewares/token.middleware.js";
+
+const router = express.Router();
+
+router.post(
+  "/sign-up",
+  [
+    body("email").isEmail().withMessage("Email is invalid"),
+    body("fullName").notEmpty().withMessage("Full name is required"),
+    body("password")
+      .isLength({ min: 8 })
+      .withMessage("Password must be at least 8 characters"),
+  ],
+  requsetHandler.validate,
+  usersController.signUp
+);
+
+router.post(
+  "/sign-in",
+  [
+    body("email").isEmail().withMessage("Email is invalid"),
+    body("password")
+      .isLength({ min: 8 })
+      .withMessage("Password must be at least 8 characters"),
+  ],
+  requsetHandler.validate,
+  usersController.signIn
+);
+
+router.get("/profile", tokenMiddleware.auth, usersController.getProfile);
+
+export default router;
